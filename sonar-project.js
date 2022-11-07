ed2d09720010909aa2ea18486bd091d9071afc26
ronaks9065 <115705180+ronaks9065@users.noreply.github.com>
2022-11-07 16:04:49 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -4,9 +4,9 @@ sonarqubeScanner({
   serverUrl: 'http://192.168.1.5:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'nodejs-application',
-	    'sonar.projectKey':'nodejs-application',
-	    'sonar.login': '261db4befc8c732b992ec7650b48aaf59c86b10b',
+	    'sonar.projectName': 'nodejs-app',
+	    'sonar.projectKey':'nodejs-app',
+	    'sonar.login': '002bef5a62dc0ba6915db4ee67d95e15ad6df473',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
